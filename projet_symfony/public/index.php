09aa5aa22a7da5bf0688ee184d2eb3c45c94b171
salma hamidallah <basmaboulli2003@gmail.com>
2024-08-27 22:04:56 +0200

    devlop

--- projet_symfony/public/index.php
+++ projet_symfony/public/index.php
@@ -4,6 +4,9 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+echo 'test'; // Ajoutez cette ligne pour afficher "test" 
+exit;       // Arrête l'exécution du script ici
+
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
